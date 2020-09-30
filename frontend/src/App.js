dbda1ce8b220fc3d0dc5ec270c7ea03636a7c523
R Narayan <nr.rnarayan@gmail.com>
2020-10-01 02:26:58 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './Components/Navbar/Navbar';
 function App() {
     return (
         <div className="App">
+            <Navbar />
             <main>Hello World</main>
         </div>
     );
