9e1140f952ae06d9f344723af03e389e39cd97bd
R Narayan <nr.rnarayan@gmail.com>
2020-10-01 08:11:44 +0530

    Some cleaning and product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import { Container } from 'react-bootstrap';
 import Header from './Components/Navbar';
 import Footer from './Components/Footer';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './bootstrap.min.css';
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main className="py-3">
                 <Container>
-                    <HomeScreen />
+                    <Route path="/" exact component={HomeScreen} />
+                    <Route path="/product/:id" component={ProductScreen} />
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 };
 
